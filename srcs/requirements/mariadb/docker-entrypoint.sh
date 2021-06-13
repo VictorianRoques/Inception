dae6ad98c13898baa101aafd317dcbd489890403
VictorianRoques <vicking421@gmail.com>
2021-06-13 16:10:02 +0200

    MARIA DB OK

---
+++ srcs/requirements/mariadb/docker-entrypoint.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+#mysql_install_db
+
+service mysql start
+
+echo "CREATE USER IF NOT EXISTS '${MYSQL_USER}'@'localhost' IDENTIFIED BY '${MYSQL_PASSWORD}';"  | mysql -u root
+echo "CREATE DATABASE IF NOT EXISTS ${MYSQL_DATABASE};" | mysql -u root
+echo "GRANT ALL PRIVILEGES ON *.* TO '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';" | mysql -u root
+echo "FLUSH PRIVILEGES;" | mysql -u root
+
+exec mysqld -u root
+
+
